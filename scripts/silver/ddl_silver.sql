2063bb0c693d1e9f53850ed1b97629dbb660fbfe
Kieran Pritchard <kj_worker@proton.me>
2026-03-24 22:03:07 +0000

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -19,8 +19,8 @@ cst_id INT,
 cst_key	NVARCHAR(50),
 cst_firstname NVARCHAR(50),	
 cst_lastname NVARCHAR(50),	
-cst_marital_status CHAR,
-cst_gndr CHAR,
+cst_marital_status NVARCHAR(50),
+cst_gndr NVARCHAR(50),
 cst_create_date DATE,
 dwh_create_date DATETIME DEFAULT GETDATE()
 
